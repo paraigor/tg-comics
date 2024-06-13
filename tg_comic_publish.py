81f692c5d2c3b16193ed0fee602b77e5bd6d099f
paraigor <103633539+paraigor@users.noreply.github.com>
2024-06-13 22:52:39 +0300

    Update code

--- tg_comic_publish.py
+++ tg_comic_publish.py
@@ -10,12 +10,47 @@ from dotenv import load_dotenv
 TG_IMG_SIZE_LIMIT = 20971520
 
 
+def get_last_comic_num():
+    comic_url = "https://xkcd.com/info.0.json"
+    response = requests.get(comic_url)
+    response.raise_for_status()
+    return response.json()["num"]
+
+
+def get_rand_comic():
+    last_comic_number = get_last_comic_num()
+    rand_comic_number = random.randint(1, last_comic_number)
+
+    comic_url = f"https://xkcd.com/{rand_comic_number}/info.0.json"
+    response = requests.get(comic_url)
+    response.raise_for_status()
+
+    img_url = response.json()["img"]
+    transcript_text = response.json()["transcript"]
+    alt_text = response.json()["alt"]
+
+    return img_url, transcript_text, alt_text
+
+
 def download_image(link, img_path):
     response = requests.get(link)
     response.raise_for_status()
 
     with open(img_path, "wb") as file:
         file.write(response.content)
+
+
+def send_comic_tg(bot, chat_id, comic):
+    img_path, transcript_text, alt_text = comic
+    if img_path.stat().st_size <= TG_IMG_SIZE_LIMIT:
+        with open(img_path, "rb") as file:
+            bot.send_document(chat_id=chat_id, document=file)
+        if transcript_text:
+            bot.send_message(chat_id=chat_id, text=transcript_text)
+        else:
+            bot.send_message(chat_id=chat_id, text=alt_text)
+    else:
+        raise Exception
 
 
 def main():
@@ -25,36 +60,20 @@ def main():
 
     tg_bot = telegram.Bot(token=tg_token)
 
-    last_comic_url = "https://xkcd.com/info.0.json"
-    last_response = requests.get(last_comic_url)
-    last_response.raise_for_status()
-
-    last_comic_number = last_response.json()["num"]
-    rand_comic_number = random.randint(1, last_comic_number)
-
-    comic_url = f"https://xkcd.com/{rand_comic_number}/info.0.json"
-    response = requests.get(comic_url)
-    response.raise_for_status()
-
-    img_url = response.json()["img"]
+    img_url, transcript_text, alt_text = get_rand_comic()
     img_dir = Path(__file__).cwd()
     img_path = img_dir / Path(img_url).name
-    transcript_text = response.json()["transcript"]
-    alt_text = response.json()["alt"]
 
     download_image(img_url, img_path)
 
-    if img_path.stat().st_size <= TG_IMG_SIZE_LIMIT:
-        with open(img_path, "rb") as file:
-            tg_bot.send_document(chat_id=tg_channel_id, document=file)
-        if transcript_text:
-            tg_bot.send_message(chat_id=tg_channel_id, text=transcript_text)
-        else:
-            tg_bot.send_message(chat_id=tg_channel_id, text=alt_text)
-        img_path.unlink(missing_ok=True)
-    else:
-        img_path.unlink(missing_ok=True)
+    comic = (img_path, transcript_text, alt_text)
+
+    try:
+        send_comic_tg(tg_bot, tg_channel_id, comic)
+    except Exception:
         sys.exit("Current image is too big for Telegram")
+    finally:
+        img_path.unlink(missing_ok=True)
 
 
 if __name__ == "__main__":
